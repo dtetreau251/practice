9663f08a383d61e0aa1cb1b96677c97de00e9547
David Tetreau <david_tetreau@yahoo.com>
2020-10-21 19:26:23 -0500

    initial commit

---
+++ colossal-adventure-console-rpg/app.js
@@ -0,0 +1,68 @@
+console.log("***Welcome to Colossal Adventure RPG***")
+
+let enemyNames = ['conquest', 'war', 'famine', 'plague']
+
+let inventory = []
+
+
+function wantToPlay() {
+    var choice = prompt("Do you want to play a game? Type 'yes' or 'no'. ")
+    if (choice === "yes") {
+        gameStart()
+    }
+    else { alert("maybe next time!") }
+}
+
+function gameStart()  {
+    userName = prompt("What is your name? ")
+    
+    alert("Close this message and press F12 to find the game")
+    
+    console.log(`You awake in a strange place. It's some sort of abandoned homestead. You have no idea how you got here. Who are you? Where did you come from? Your head is splitting, and your thoughts are foggy. You finally remember...your name is ${userName}. You are dressed in trousers, a long sleeve cotton shirt, cowboy boots, and an empty holster. You look outside to see a deserted wasteland. You begin to walk...
+    Press 'w' to walk`)
+        }
+    
+
+function walk() {
+
+}
+
+function attack() {
+  
+}
+
+function run() {
+
+}
+
+function damage() {
+
+}
+
+function escape() {
+
+}
+
+function enemyAttack() {
+
+}
+
+function enemyKilled(){
+
+}
+
+function playerKilled() {
+
+}
+
+function printInventory() {
+
+}
+
+
+
+
+
+
+
+    
